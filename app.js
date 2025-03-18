3cfc3684b6243e59b0152ce82ac58fbe3511c32d
Lucaasgar <aglucasp@gmail.com>
2025-03-17 21:55:08 -0300

    last commit

--- app.js
+++ app.js
@@ -1,1 +1,45 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = []
+let lista = document.getElementById("listaAmigos")
+
+function agregarAmigo(){
+    let inputAmigo = document.getElementById("amigo");
+    let nombreAmigo = inputAmigo.value.trim()
+
+    if(nombreAmigo.length === 0){
+        alert("Por favor, inserte un nombre.")
+    } else {
+        amigos.push(nombreAmigo)
+        inputAmigo.value = "";
+        actualizarAmigos()
+    }
+
+    console.log("Amigos: ", amigos)
+}
+
+function actualizarAmigos(){
+    lista.innerHTML = ""
+    
+
+    amigos.forEach((amigo) => {
+        let nuevoAmigo = document.createElement("li")
+        nuevoAmigo.textContent = amigo
+        lista.appendChild(nuevoAmigo)
+    })
+}
+
+function sortearAmigo(){
+    let resultado = document.getElementById("resultado")
+    resultado.innerHTML = ""
+    
+    if(amigos.length === 0){
+        alert("No hay amigos. Añadelos!")
+
+    } else {
+        let ind = Math.floor(Math.random() * amigos.length)
+        let amigoSecreto = amigos[ind]
+        
+        amigos = []
+        actualizarAmigos()
+        resultado.innerHTML = amigoSecreto
+    }
+}
